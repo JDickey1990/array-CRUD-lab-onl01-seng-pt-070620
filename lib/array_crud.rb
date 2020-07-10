9079c885c298b9282b6cd0e0303e6e5f1bbdd81f
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-10 19:49:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array=[1,2,3,4]
+  4_array=create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
