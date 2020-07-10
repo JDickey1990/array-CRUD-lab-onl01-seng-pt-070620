655f56ab08c78f93c213d57d7348b95e53633066
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-10 20:01:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array=
+  create_an_array=[1,2,3,4,"am"]
+  create_an_array.first
 end
 
 def retrieve_last_element_from_array(array)
